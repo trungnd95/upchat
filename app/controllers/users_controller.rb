b4b6c24044b81f92cf8958ee0f2d536bba97d710
Trung Nguyen <ndt8895@gmail.com>
2016-10-20 16:20:59 +0700

    Sign up user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    @user  = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    respond_to do |format|
+      if @user.save
+        format.html  do
+          redirect_to root_path, flash: {success: "Signed up successfully"}
+        end
+        format.json {render json: @user, status: :created}
+      else
+        format.html do
+          flash[:error] = "Some thing went wrong!"
+          render action: :new
+        end
+        format.json{render json: @user.errors, status: :unprocessable}
+      end
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+end
